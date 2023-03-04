4b8bcdcb7a7e2bb08e938964b28c2b571282293a
github_username <email_address>
2023-03-04 19:11:29 +0530

    Shraddha

---
+++ Deepankar/src/Kshitija/Program.java
@@ -0,0 +1,15 @@
+package Kshitija;
+
+public class Program {
+	public void m1()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Program n = new Program();
+n.m1();
+	}
+
+}
